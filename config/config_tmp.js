40058a3a34350702e0b040d77b154a37d75c3933
widiskel <waputra16@gmail.com>
2024-10-07 20:46:22 +0700

    first commit

---
+++ config/config_tmp.js
@@ -0,0 +1,5 @@
+export class Config {
+  static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
+  static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
+  static AUTOPLAYTAPCOUNT = 10; // COUNT OF AUTOPLAY
+}
